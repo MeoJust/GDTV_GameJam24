016251a07fdd6b0a373fddd6ba2a6091163da9e5
MeoJust <meojust@gmail.com>
2024-06-02 20:34:53 +0300

    sbmit

--- Assets/_scripts/TilesNumerator.cs
+++ Assets/_scripts/TilesNumerator.cs
@@ -7,6 +7,8 @@ public class TilesNumerator : MonoBehaviour
     TextMeshPro _name;
     Vector2Int _coords = new Vector2Int();
 
+    [SerializeField] private Vector2 gridSnapSize = new Vector2(1f, 1f);
+
     void Start()
     {
         _name = GetComponent<TextMeshPro>();
@@ -24,8 +26,12 @@ public class TilesNumerator : MonoBehaviour
 
     void NumsView()
     {
-        _coords.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
-        _coords.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+        // _coords.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        // _coords.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+
+        _coords.x = Mathf.RoundToInt(transform.parent.position.x / gridSnapSize.x);
+        _coords.y = Mathf.RoundToInt(transform.parent.position.z / gridSnapSize.y);
+
         _name.text = _coords.x + ", " + _coords.y;
     }
 
